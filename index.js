e061067eda2cb5d983643c8e23a3027cbf1362bd
ISHITA JAIN <>
2021-10-19 22:17:05 +0530

    drum-kit-updation

---
+++ index.js
@@ -0,0 +1,59 @@
+//JAVASCRIPT
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var keySource=this.innerHTML;
+
+    getSound(keySource);
+    addAnimation(keySource);
+});
+}
+
+document.addEventListener("keydown",function(event)
+{
+getSound(event.key);
+addAnimation(event.key);
+});
+
+function getSound(key)
+{
+   switch (key) {
+      case "w":
+         var crash = new Audio("sounds/crash.mp3");
+         crash.play();
+         break;
+
+      case "a":
+         var kick = new Audio("sounds/kick-bass.mp3");
+         kick.play();
+        break;
+      case "s":
+         var snare = new Audio("sounds/snare.mp3");
+         snare.play();
+         break;
+      case "d":
+         var tom1 = new Audio("sounds/tom-1.mp3");
+         tom1.play();
+      case "j":
+         var tom2 = new Audio("sounds/tom-2.mp3");
+         tom2.play();
+      case "k":
+         var tom3 = new Audio("sounds/tom-3.mp3");
+         tom3.play();
+      case "l":
+         var tom4 = new Audio("sounds/tom-4.mp3");
+         tom4.play();
+
+      default:
+         console.log(keySource);
+   }
+}
+
+function addAnimation(currentKey){
+   var press = document.querySelector("."+currentKey);
+   press.classList.add("pressed");
+   setTimeout(function(){
+      press.classList.remove("pressed");
+   },100)
+}
+
+
